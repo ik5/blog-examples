5157aad452a17b32414063b335077a4409fe4f7c
ik5 <idok@linesip.com>
2012-04-01 13:08:12 +0300

    Initial commit

---
+++ fpc/csi ip address/unit1.pas
@@ -0,0 +1,39 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('The IP Address is 127.0.0.1');
+end;
+
+end.
+
