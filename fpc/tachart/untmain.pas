750110e12fd64bcb2111176b9fa1617b12e9e4f7
ik5 <idok@linesip.com>
2012-09-18 14:18:56 +0300

    Renamed names of components

--- fpc/tachart/untmain.pas
+++ fpc/tachart/untmain.pas
@@ -13,26 +13,26 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    Button1: TButton;
-    Button2: TButton;
-    Button3: TButton;
-    Button4: TButton;
-    Chart1: TChart;
-    ColorDialog1: TColorDialog;
-    Panel1: TPanel;
-    SaveDialog1: TSaveDialog;
-    Splitter1: TSplitter;
-    StringGrid1: TStringGrid;
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
-    procedure Button3Click(Sender: TObject);
-    procedure Button4Click(Sender: TObject);
+    btnAddItem: TButton;
+    btnRemoveLastItem: TButton;
+    btnApply: TButton;
+    btnExport: TButton;
+    Chart: TChart;
+    ColorDialog: TColorDialog;
+    pnlBottom: TPanel;
+    SaveDialog: TSaveDialog;
+    spltBottom: TSplitter;
+    sgGraphDetails: TStringGrid;
+    procedure btnAddItemClick(Sender: TObject);
+    procedure btnRemoveLastItemClick(Sender: TObject);
+    procedure btnApplyClick(Sender: TObject);
+    procedure btnExportClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-    procedure StringGrid1ButtonClick(Sender: TObject; aCol, aRow: Integer);
-    procedure StringGrid1ColRowDeleted(Sender: TObject; IsColumn: Boolean;
+    procedure sgGraphDetailsButtonClick(Sender: TObject; aCol, aRow: Integer);
+    procedure sgGraphDetailsColRowDeleted(Sender: TObject; IsColumn: Boolean;
       sIndex, tIndex: Integer);
-    procedure StringGrid1SetEditText(Sender: TObject; ACol, ARow: Integer;
+    procedure sgGraphDetailsSetEditText(Sender: TObject; ACol, ARow: Integer;
       const Value: string);
   private
     { private declarations }
@@ -53,28 +53,28 @@ uses TATextElements, TAChartUtils, TALegend, TADrawerSVG, TADrawUtils,
 
 { TForm1 }
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TForm1.btnAddItemClick(Sender: TObject);
 begin
-  StringGrid1.RowCount := StringGrid1.RowCount + 1;
-  StringGrid1.Cells[0,StringGrid1.RowCount -1] := IntToStr(StringGrid1.RowCount -1);
+  sgGraphDetails.RowCount := sgGraphDetails.RowCount + 1;
+  sgGraphDetails.Cells[0,sgGraphDetails.RowCount -1] := IntToStr(sgGraphDetails.RowCount -1);
 
 end;
 
-procedure TForm1.Button2Click(Sender: TObject);
+procedure TForm1.btnRemoveLastItemClick(Sender: TObject);
 begin
-  if StringGrid1.RowCount > 1 then
-    StringGrid1.RowCount := StringGrid1.RowCount - 1;
+  if sgGraphDetails.RowCount > 1 then
+    sgGraphDetails.RowCount := sgGraphDetails.RowCount - 1;
 end;
 
-procedure TForm1.Button3Click(Sender: TObject);
+procedure TForm1.btnApplyClick(Sender: TObject);
 var i    : integer;
     acol : TColor;
 begin
   if not Assigned(pie) then initpie; // first time applying ...
   pie.Clear; // clear existing items
-  for i := 1 to StringGrid1.RowCount -1 do
+  for i := 1 to sgGraphDetails.RowCount -1 do
    begin
-     with StringGrid1 do
+     with sgGraphDetails do
       begin
         if Cells[2, i] = '' then continue; // empty field
         acol := clTAColor; // set color automatic
@@ -87,25 +87,25 @@ begin
    end;
 end;
 
-procedure TForm1.Button4Click(Sender: TObject);
+procedure TForm1.btnExportClick(Sender: TObject);
 var fs : TFileStream;
     id : IChartDrawer;
 begin
   // Does the user want to save an image of the chart ?
-  if not SaveDialog1.Execute then exit;
+  if not SaveDialog.Execute then exit;
 
-  case SaveDialog1.FilterIndex of // lets see what type of image they want
-    1 : Chart1.SaveToBitmapFile(SaveDialog1.FileName);                          // bitmap
-    2 : Chart1.SaveToFile(TJPEGImage, SaveDialog1.FileName);                    // JPeg
-    3 : Chart1.SaveToFile(TPortableNetworkGraphic, SaveDialog1.FileName);       // PNG - our default
-    4 : Chart1.SaveToFile(TPortableAnyMapGraphic, SaveDialog1.FileName);        // PPM
-    5 : Chart1.SaveToFile(TPixmap, SaveDialog1.FileName);                       // XPM
+  case SaveDialog.FilterIndex of // lets see what type of image they want
+    1 : Chart.SaveToBitmapFile(SaveDialog.FileName);                          // bitmap
+    2 : Chart.SaveToFile(TJPEGImage, SaveDialog.FileName);                    // JPeg
+    3 : Chart.SaveToFile(TPortableNetworkGraphic, SaveDialog.FileName);       // PNG - our default
+    4 : Chart.SaveToFile(TPortableAnyMapGraphic, SaveDialog.FileName);        // PPM
+    5 : Chart.SaveToFile(TPixmap, SaveDialog.FileName);                       // XPM
     6 : begin                                                                   // SVG
-          fs := TFileStream.Create(SaveDialog1.FileName, fmCreate);  //create a stream
+          fs := TFileStream.Create(SaveDialog.FileName, fmCreate);  //create a stream
           try
             id                       := TSVGDrawer.Create(fs,true);  // create an svg drawer
             id.DoChartColorToFPColor := @ChartColorSysToFPColor;     // set the default color chart of the system
-            Chart1.Draw(id, Rect(0,0, Chart1.Width, Chart1.Height)); // draw it to the canvas
+            Chart.Draw(id, Rect(0,0, Chart.Width, Chart.Height)); // draw it to the canvas
           finally
             fs.Free; // free the file
           end;
@@ -116,10 +116,10 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   // Add the '#' string to the first fixed item
-  StringGrid1.Cells[0,0] := '#';
+  sgGraphDetails.Cells[0,0] := '#';
   // Disable Delete and Apply buttons. we do not have any content yet
-  Button2.Enabled        := StringGrid1.RowCount > 1;
-  Button3.Enabled        := StringGrid1.RowCount > 1;
+  btnRemoveLastItem.Enabled        := sgGraphDetails.RowCount > 1;
+  btnApply.Enabled        := sgGraphDetails.RowCount > 1;
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
@@ -128,26 +128,26 @@ begin
     FreeAndNil(pie);
 end;
 
-procedure TForm1.StringGrid1ButtonClick(Sender: TObject; aCol, aRow: Integer);
+procedure TForm1.sgGraphDetailsButtonClick(Sender: TObject; aCol, aRow: Integer);
 begin
   if (aRow >= 1) and (aCol = 3) then // are we at the last column ?
     begin
-      if ColorDialog1.Execute then // if the user selected a color, apply it
+      if ColorDialog.Execute then // if the user selected a color, apply it
         begin
-          StringGrid1.Cells[aCol, aRow] := ColorToString(ColorDialog1.Color);
+          sgGraphDetails.Cells[aCol, aRow] := ColorToString(ColorDialog.Color);
         end;
     end;
 end;
 
-procedure TForm1.StringGrid1ColRowDeleted(Sender: TObject; IsColumn: Boolean;
+procedure TForm1.sgGraphDetailsColRowDeleted(Sender: TObject; IsColumn: Boolean;
   sIndex, tIndex: Integer);
 begin
   // Decide if we need to enable or not the delete and apply buttons ...
-  Button2.Enabled := StringGrid1.RowCount > 1;
-  Button3.Enabled := StringGrid1.RowCount > 1;
+  Button2.Enabled := sgGraphDetails.RowCount > 1;
+  btnApply.Enabled := sgGraphDetails.RowCount > 1;
 end;
 
-procedure TForm1.StringGrid1SetEditText(Sender: TObject; ACol, ARow: Integer;
+procedure TForm1.sgGraphDetailsSetEditText(Sender: TObject; ACol, ARow: Integer;
   const Value: string);
 var num : double;
 begin
@@ -156,15 +156,15 @@ begin
     begin
       if not TryStrToFloat(Value, num) then // do we have a real floating point number ?
         begin
-          StringGrid1.Cells[ACol, ARow] := ''; // do not save the value if not ...
+          sgGraphDetails.Cells[ACol, ARow] := ''; // do not save the value if not ...
         end;
     end;
 end;
 
 procedure TForm1.initpie;
 begin
-  Chart1.ClearSeries; // remove any existed series
-  pie          := TPieSeries.Create(Chart1); // Create a pie series
+  Chart.ClearSeries; // remove any existed series
+  pie          := TPieSeries.Create(Chart); // Create a pie series
   pie.Title    := 'Pie Chart'; // Give it a name
   pie.Exploded := False; // don't allow each part to be apart from the pie
   with pie.Legend do
@@ -183,11 +183,11 @@ begin
      Visible           := true;
    end;
 
-  with Chart1.Legend do // control the legend
+  with Chart.Legend do // control the legend
    begin
      GroupTitles.Clear;
      GroupTitles.Add('Pie');
-     Chart1.AddSeries(pie); // add the series to the chart
+     Chart.AddSeries(pie); // add the series to the chart
    end;
 end;
 
